c6588d558681574cf386e7a30ac9e45d643feafb
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-04-24 18:11:31 +0530

    added frequently asked question

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,42 @@
 
 const Home = () => {
     return(
-    <div className="Offers">
-      <div className="Offerhead">
-        <h1>What Network Offers</h1>
-        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
+      <div className="Home">
+        <div className="Offers">
+          <div className="Offerhead">
+            <h1>What Network Offers</h1>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
+          </div>
+          <div className="offerpts">
+            <h4>Connects you to the right person</h4>
+            <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</h6>
+            <h4>Map based interface</h4>
+            <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</h6>
+            <h4>Safe and Secure Services</h4>
+            <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</h6>
+          </div>
+        </div>
+        <div className="freq">
+          <div className="freqhead">
+            <h1>Frequently asked Questions</h1>
+            <h6>Find answers to commonly asked questions about our platform and services.</h6>
+            <button>Contact</button>
+          </div>
+          <div className="freqques">
+            <hr></hr>
+            <h4>How does it work?</h4>
+            <hr></hr>
+            <h4>Is it safe?</h4>
+            <hr></hr>
+            <h4>How do I sign Up?</h4>
+            <hr></hr>
+            <h4>Can I offer my services?</h4>
+            <hr></hr>
+            <h4>How can I get paid?</h4>
+            <hr></hr>
+          </div>
+        </div>
       </div>
-      <div className="offerpts">
-        <h4>Connects you to the right person</h4>
-        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</h6>
-        <h4>Map based interface</h4>
-        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</h6>
-        <h4>Safe and Secure Services</h4>
-        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</h6>
-      </div>
-    </div>
     );
   };
   
