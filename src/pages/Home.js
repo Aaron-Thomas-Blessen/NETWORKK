fe687452957f68220ad8b58bb13b0616c70294d6
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-04-24 18:00:48 +0530

    Network offers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
 
 const Home = () => {
     return(
-    <div>Home</div>
+    <div className="Offers">
+      <div className="Offerhead">
+        <h1>What Network Offers</h1>
+        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
+      </div>
+      <div className="offerpts">
+        <h4>Connects you to the right person</h4>
+        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</h6>
+        <h4>Map based interface</h4>
+        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</h6>
+        <h4>Safe and Secure Services</h4>
+        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</h6>
+      </div>
+    </div>
     );
   };
   
