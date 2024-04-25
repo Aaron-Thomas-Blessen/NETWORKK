72f4dea813be34084d9e6675ac24dd1f4a1e3633
AKSHAY GOPAN <akshaygopan377@gmail.com>
2024-04-25 14:03:33 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,40 @@
-
+import Navbar from "../components/nav";
+import Carousel from "../components/carousel";
 const Home = () => {
+
+  const images = [
+    'https://picsum.photos/1980/1080',
+    'https://picsum.photos/1980/1080',
+    'https://picsum.photos/1980/1080',
+    // Add more image URLs as needed
+  ];
+
+
+const TwoColumnSection = () => {
+  return (
+    <div className="two-column-section">
+      <div className="left-column">
+      <h4>Discover</h4>
+      <h1>Find Skilled Workers and <br/>Offer Your Services</h1>
+      <div className="btn">
+        <button>Get Started</button>
+        <p>Learn more</p>
+      </div>
+      </div>
+      <div className="right-column">
+
+      </div>
+    </div>
+  );
+};
+
+
     return(
-    <div>Home</div>
+    <div className="home">
+      <Navbar />
+      <Carousel images={images} />
+      <TwoColumnSection />
+    </div>
     );
   };
   
