2b4ca12580186693f37b35679f5d4eb34f5df888
AKSHAY GOPAN <akshaygopan377@gmail.com>
2024-04-27 14:30:46 +0530

    Update carousel.js

---
+++ src/components/carousel.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+
+
+const Carousel = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
+  return (
+    <div className="carousel-container">
+      <div className="carousel">
+        <div
+          className="carousel-inner"
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+        >
+          {images.map((image, index) => (
+            <div key={index} className="carousel-slide">
+              <img src={image} alt={`Slide ${index + 1}`} />
+            </div>
+          ))}
+        </div>
+        <button className="carousel-btn prev" onClick={prevSlide}>
+          &lt;
+        </button>
+        <button className="carousel-btn next" onClick={nextSlide}>
+          &gt;
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
