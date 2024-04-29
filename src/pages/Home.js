97e3792573e3b20ca42d9454dc1e445dfdaeacf7
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-04-29 21:09:15 +0530

    css updated for logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,87 +1,112 @@
-
 const Home = () => {
-    return(
-      <div className="Home">
-        <div className="Offers">
-          <div className="Offerhead">
-            <h1>What Network Offers</h1>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
+  return (
+    <div className="Home">
+      <div className="Offers">
+        <div className="Offerhead">
+          <h1>What Network Offers</h1>
+          <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
+        </div>
+        <div className="offerpts">
+          <h4>Connects you to the right person</h4>
+          <h6>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim in eros elementum tristique.
+          </h6>
+          <h4>Map based interface</h4>
+          <h6>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim in eros elementum tristique.
+          </h6>
+          <h4>Safe and Secure Services</h4>
+          <h6>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim in eros elementum tristique.
+          </h6>
+        </div>
+      </div>
+      <div className="freq">
+        <div className="freqhead">
+          <h1>Frequently asked Questions</h1>
+          <h6>
+            Find answers to commonly asked questions about our platform and
+            services.
+          </h6>
+          <button>Contact</button>
+        </div>
+        <div className="freqques">
+          <hr></hr>
+          <h4>How does it work?</h4>
+          <hr></hr>
+          <h4>Is it safe?</h4>
+          <hr></hr>
+          <h4>How do I sign Up?</h4>
+          <hr></hr>
+          <h4>Can I offer my services?</h4>
+          <hr></hr>
+          <h4>How can I get paid?</h4>
+          <hr></hr>
+        </div>
+      </div>
+      <div className="Team">
+        <div className="Teamhead">
+          <h6>Connecting</h6>
+          <h1>Our Team</h1>
+          <h6>Meet the talented individuals behind Networkk</h6>
+        </div>
+        <div className="Teamname">
+          <div className="Aaron">
+            <h2>Aaron Thomas Blessen</h2>
+            <h3>CMO</h3>
+            <h4>
+              Experienced strategist with a passion for innovation and growth.
+            </h4>
+            <button>LinkedIn</button>
+            <button>Twitter</button>
+            <button>Portfolio</button>
           </div>
-          <div className="offerpts">
-            <h4>Connects you to the right person</h4>
-            <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</h6>
-            <h4>Map based interface</h4>
-            <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</h6>
-            <h4>Safe and Secure Services</h4>
-            <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</h6>
+          <div className="Akshay">
+            <h2>Akshay Gopan</h2>
+            <h3>CTO</h3>
+            <h4>
+              Tech enthusiast with expertise in developing cutting-edge
+              solutions.
+            </h4>
+            <button>LinkedIn</button>
+            <button>Twitter</button>
+            <button>Portfolio</button>
+          </div>
+          <div className="Alan">
+            <h2>Alan Philip</h2>
+            <h3>COO</h3>
+            <h4>
+              Operations expert focused on delivering exceptional user
+              experiences.
+            </h4>
+            <button>LinkedIn</button>
+            <button>Twitter</button>
+            <button>Portfolio</button>
+          </div>
+          <div className="Chand">
+            <h2>Chandrasekhar C.A</h2>
+            <h3>Marketing Director</h3>
+            <h4>
+              Strategic marketer driving brand awareness and customer
+              acquisition.
+            </h4>
+            <button>LinkedIn</button>
+            <button>Twitter</button>
+            <button>Portfolio</button>
           </div>
         </div>
-        <div className="freq">
-          <div className="freqhead">
-            <h1>Frequently asked Questions</h1>
-            <h6>Find answers to commonly asked questions about our platform and services.</h6>
-            <button>Contact</button>
-          </div>
-          <div className="freqques">
-            <hr></hr>
-            <h4>How does it work?</h4>
-            <hr></hr>
-            <h4>Is it safe?</h4>
-            <hr></hr>
-            <h4>How do I sign Up?</h4>
-            <hr></hr>
-            <h4>Can I offer my services?</h4>
-            <hr></hr>
-            <h4>How can I get paid?</h4>
-            <hr></hr>
-          </div>
-        </div>
-        <div className="Team">
-          <div className="Teamhead">
-            <h6>Connecting</h6>
-            <h1>Our Team</h1>
-            <h6>Meet the talented individuals behind Networkk</h6>
-          </div>
-          <div className="Teamname">
-            <div className="Aaron">
-              <h2>Aaron Thomas Blessen</h2>
-              <h3>CMO</h3>
-              <h4>Experienced strategist with a passion for innovation and growth.</h4>
-              <button>LinkedIn</button>
-              <button>Twitter</button>
-              <button>Portfolio</button>
-            </div>
-            <div className="Akshay">
-              <h2>Akshay Gopan</h2>
-              <h3>CTO</h3>
-              <h4>Tech enthusiast with expertise in developing cutting-edge solutions.</h4>
-              <button>LinkedIn</button>
-              <button>Twitter</button>
-              <button>Portfolio</button>
-            </div>
-            <div className="Alan">
-              <h2>Alan Philip</h2>
-              <h3>COO</h3>
-              <h4>Operations expert focused on delivering exceptional user experiences.</h4>
-              <button>LinkedIn</button>
-              <button>Twitter</button>
-              <button>Portfolio</button>
-            </div>
-            <div className="Chand">
-              <h2>Chandrasekhar C.A</h2>
-              <h3>Marketing Director</h3>
-              <h4>Strategic marketer driving brand awareness and customer acquisition.</h4>
-              <button>LinkedIn</button>
-              <button>Twitter</button>
-              <button>Portfolio</button>
-            </div>
-          </div>
-        </div>
-        <div className="contact">
-          <div className="contacthead">
+      </div>
+      <div className="contact">
+        <div className="contacthead">
           <div className="contactus">
             <h1>Contact Us</h1>
-            <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</h6>
+            <h6>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse varius enim in eros elementum tristique.
+            </h6>
           </div>
           <div class="contactdet">
             <h3>Email</h3>
@@ -91,36 +116,44 @@ const Home = () => {
             <h3>Office</h3>
             <h6>123 Sample St, Sydney NSW 2000 AU</h6>
           </div>
-          </div>
-          <div className="contactmap">
-            map here
-          </div>
         </div>
-        <div className="news">
-          <div className="newshead">
-            <h2>LOGO</h2>
-            <h5>Stay up to date on features and releases by joining our newsletter.</h5>
-            <input type="email" placeholder="Your Email"></input>
-            <button>Subscribe</button>
-            <h6>By subscribing, you agree to our Privacy Policy and consent to receive updates from our company.</h6>
-          </div>
-          <div className="newsfollow">
-            <h3>Follow us</h3>
-            <h4>Facebook</h4>
-            <h4>Instagram</h4>
-            <h4>Twitter</h4>
-            <h4>LinkedIn</h4>
-            <h4>Youtube</h4>
-          </div>
-        </div>
-        <hr></hr>
-        <h6>© 2024 Networkk  All rights reserved.</h6>
-        <h6>Privacy Policy</h6>
-        <h6>Terms of Service</h6>
-        <h6>Cookies Settings</h6>
+        <div className="contactmap">map here</div>
       </div>
-    );
-  };
-  
-  export default Home;
-  
+      <div className="news">
+        <div className="newshead">
+          <h2>LOGO</h2>
+          <h5>
+            Stay up to date on features and releases by joining our newsletter.
+          </h5>
+          <input type="email" placeholder="Your Email"></input>
+          <button>Subscribe</button>
+          <h6>
+            By subscribing, you agree to our Privacy Policy and consent to
+            receive updates from our company.
+          </h6>
+        </div>
+        <div className="newsfollow">
+          <h3>Follow us</h3>
+          <h4>Facebook</h4>
+          <h4>Instagram</h4>
+          <h4>Twitter</h4>
+          <h4>LinkedIn</h4>
+          <h4>Youtube</h4>
+        </div>
+      </div>
+      <hr></hr>
+      <div class="footer">
+        <div class="left">
+          <h6>© 2024 Networkk All rights reserved.</h6>
+        </div>
+        <div class="right">
+          <h6>Privacy Policy</h6>
+          <h6>Terms of Service</h6>
+          <h6>Cookies Settings</h6>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
