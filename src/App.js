d44c21e4aea5392f5bb227e7e5446ddbe6488680
AKSHAY GOPAN <akshaygopan377@gmail.com>
2024-04-30 15:41:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Service from "./pages/Service";
 import Navbar from "./components/nav";
+import UserGigView from "./pages/user-gigs-views";
 
 
 const App = () => {
 
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/Service" element={<Service />}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
-      </Routes>
-    </BrowserRouter>
-    
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home />}></Route>
+    //     <Route path="/Service" element={<Service />}></Route>
+    //     <Route path="/dashboard" element={<Dashboard />}></Route>
+    //   </Routes>
+    // </BrowserRouter>
+    <UserGigView />    
 
   );
 };
