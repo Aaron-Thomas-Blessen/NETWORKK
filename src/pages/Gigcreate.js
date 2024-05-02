7aa9834b0b25858f2055674c003f16224842a790
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-05-02 23:25:05 +0530

    gigcreate completed

--- src/pages/Gigcreate.js
+++ src/pages/Gigcreate.js
@@ -1,41 +1,48 @@
 import Navbar from "../components/nav";
 
 const Gigcreate = () => {
-    const Gig = () =>{
-        return(
-            <div className="gig">
-                <div className="gigcreate">
-                    <h1>Create your Gig</h1>
-                </div>
-                <div className="gigtitle">
-                    <input type="text" placeholder="Enter your Gig title"></input>
-                </div>
-                <div className="gigcat">
-                    <input type="text" placeholder="Enter your Gig Category"></input>
-                </div>
-                <div className="gigsearch">
-                    <input type="text" placeholder="Enter Search Tags"></input>
-                </div>
-                <div className="gigdes">
-                    <input type="text" placeholder="enter Description"></input>
-                </div>
-                <div className="gigperv">
-                    <input type="text" placeholder="enter Description"></input>
-                </div>
-                <div className="gigtitle">
-                    <input type="text" placeholder="enter you gig title"></input>
+    const Gig = () => {
+        return (
+            <div className="flex flex-col items-center justify-center">
+                <div className="gig mb-8 w-96">
+                    <div className="gigcreate">
+                        <h1 className="text-3xl text-center mb-4">Create your Gig</h1>
+                    </div>
+                    <div className="gigtitle mb-4">
+                        <label htmlFor="1" className="block">Gig Title</label>
+                        <input type="text" id="1" placeholder="Enter your Gig title" className="border-black border min-w-full px-4 py-2"></input>
+                    </div>
+                    <div className="gigcat mb-4">
+                        <label htmlFor="2" className="block">Gig Category</label>
+                        <input type="text" id="2" placeholder="Enter your Gig Category" className="border-black border min-w-full px-4 py-2"></input>
+                    </div>
+                    <div className="gigsearch mb-4">
+                        <label htmlFor="3" className="block">Search tags</label>
+                        <input type="text" id="3" placeholder="Enter Search Tags" className="border-black border min-w-full px-4 py-2"></input>
+                    </div>
+                    <div className="gigdes mb-4">
+                        <label htmlFor="4" className="block">Description</label>
+                        <input type="text" id="4" placeholder="enter Description" className="border-black border min-w-full px-4 py-2"></input>
+                    </div>
+                    <div className="gigperv mb-4">
+                        <div className="">
+                            <label htmlFor="5" className="block">Previous Works</label>
+                            <input className="border border-black p-4 w-full px-4 py-2" type="file" id="5" accept="image/png, image/jpeg"></input>
+                        </div>
+                    </div>
+                    <div className="gigsubmit flex justify-center">
+                        <input type="submit" value="Publish Gig" className="bg-gray-500 text-white px-6 py-3 rounded-lg hover:bg-gray-600 cursor-pointer text-lg"></input>
+                    </div>
                 </div>
             </div>
         );
     }
     return (
         <div className="Gigcreate">
-          <Navbar />
-          <Gig />
+            <Navbar />
+            <Gig />
         </div>
-      );
-  };
-  
-  export default Gigcreate;
-  
+    );
+};
 
+export default Gigcreate;
