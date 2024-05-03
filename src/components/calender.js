0ad334066495653df83e4960b29d8cdcf6b118fa
AKSHAY GOPAN <akshaygopan377@gmail.com>
2024-05-03 11:57:55 +0530

    Update calender.js

--- src/components/calender.js
+++ src/components/calender.js
@@ -2,10 +2,10 @@ import React, { useEffect, useRef } from 'react';
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
-const calStyle = {
-    height: "500px",
-    width: "500px"
-};
+// const calStyle = {
+//     height: "500px",
+//     width: "500px"
+// };
 const MyCalendar = () => {
   const calendarRef = useRef(null);
 
@@ -13,13 +13,13 @@ const MyCalendar = () => {
     const calendar = new Calendar(calendarRef.current, {
       plugins: [dayGridPlugin],
       initialView: 'dayGridMonth',
-      initialDate: '2024-04-01', // Set the initial date to April 1st, 2024
+      initialDate: '2024-05-01', // Set the initial date to April 1st, 2024
       fixedWeekCount: true,
-      events: [
-        { title: 'Event 1', date: '2024-04-01' },
-        { title: 'Event 2', date: '2024-04-05' },
-        { title: 'Event 3', date: '2024-04-10' },
-      ],
+      // events: [
+      //   { title: 'Event 1', date: '2024-04-01' },
+      //   { title: 'Event 2', date: '2024-04-05' },
+      //   { title: 'Event 3', date: '2024-04-10' },
+      // ],
     });
 
     calendar.render();
@@ -29,7 +29,7 @@ const MyCalendar = () => {
     };
   }, []);
 
-  return <div style={calStyle} className="bg-slate-200 rounded-lg shadow-lg overflow-hidden" ref={calendarRef} />;
+  return <div className="bg-slate-200 rounded-lg overflow-hidden p-2" ref={calendarRef} />;
 };
 
 export default MyCalendar;
