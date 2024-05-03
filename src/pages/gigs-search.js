8705ce8bc3216d56a4d95bea1b0956e3924b0d24
AKSHAY GOPAN <akshaygopan377@gmail.com>
2024-05-03 16:45:16 +0530

    Update gigs-search.js

--- src/pages/gigs-search.js
+++ src/pages/gigs-search.js
@@ -1,4 +1,21 @@
-import { SlMagnifier } from "react-icons/sl";
+import { SlMagnifier, SlStar } from "react-icons/sl";
+
+
+const HingeSign = ({rating, name, review}) => (
+    <div className="bg-white border p-2 w-64 rounded-xl shadow-md">
+        <img className="w-full h-40"></img>
+        
+        <div className="flex bg-slate-100 mt-5 w-12 h-auto p-1 align-middle">
+            <p className="text-base">{rating}</p>
+            <SlStar className="m-1 fill-amber-400"/>
+        </div>
+        <div className="p-1">
+        <p className="text-gray-700 mb-4">I can fix anything</p>
+        <p className="  text-gray-900">From Rs. 200</p>
+        </div>
+    </div>
+  );
+
 
 const GigsSearch = () => {
   return (
@@ -8,19 +25,32 @@ const GigsSearch = () => {
         </div>
 
         <div className="flex flex-row w-full justify-center mt-16 ">
-        <input
+            <input
                 type="text"
                 placeholder="What are you looking for..."
-                className="border border-gray-400 rounded-l-md py-2 px-4 w-96 focus:outline-none focus:ring-1 focus:ring-slate-950"
-            />
+                className="border border-gray-400 rounded-l-md py-2 px-4 w-96 focus:outline-none focus:ring-1 focus:ring-slate-950" />
             <button
                 type="button"
-                className="bg-black text-white py-2 px-4 w-12 rounded-r-md "
-            >
+                className="bg-slate-700 text-white py-2 px-4 w-12 rounded-r-md ">
                 <SlMagnifier />
             </button>
 
         </div> 
+            
+        <div className="flex flex-row justify-evenly mt-14">
+            <HingeSign rating={4.5}/>
+            <HingeSign rating={3.5}/>
+            <HingeSign rating={4.2}/>
+            <HingeSign rating={4.0}/>
+            <HingeSign rating={3.8}/>
+        </div>
+        <div className="flex flex-row justify-evenly mt-14">
+            <HingeSign rating={4.5}/>
+            <HingeSign rating={3.5}/>
+            <HingeSign rating={4.2}/>
+            <HingeSign rating={4.0}/>
+            <HingeSign rating={3.8}/>
+        </div>
         
     </>
   );
