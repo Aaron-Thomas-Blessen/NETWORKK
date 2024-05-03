c9255a4cb05c329d4622edd8563fa9e985a56d7f
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-05-03 22:18:23 +0530

    navbar user

--- src/pages/SellerProfilePage.js
+++ src/pages/SellerProfilePage.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import SellerProfileForm from '../components/SellerProfileForm'; // Adjusted import path for SellerProfileForm
+import React from "react";
+import SellerProfileForm from "../components/SellerProfileForm"; // Adjusted import path for SellerProfileForm
 import Navsign from "../components/navsign";
 
 const SellerProfilePage = () => {
   const Seller = () => {
+    return (
+      <div className="mx-auto max-w-md p-6 bg-white shadow-md rounded-md">
+        <h2 className="text-2xl font-semibold mb-4">Seller Profile</h2>
+        <SellerProfileForm />
+      </div>
+    );
+  };
   return (
-    <div className="mx-auto max-w-md p-6 bg-white shadow-md rounded-md">
-      <h2 className="text-2xl font-semibold mb-4">Seller Profile</h2>
-      <SellerProfileForm />
-    </div>
-  );
-}
-return(
-  <div className="Gigcreate">
+    <div className="Gigcreate">
       <Navsign />
       <Seller />
-  </div>
-);
-}
+    </div>
+  );
+};
 
 export default SellerProfilePage;
