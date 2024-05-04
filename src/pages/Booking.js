212a24457f1a0d6d4d8a086caa0f446bef8b1cfa
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-05-04 15:54:23 +0530

    booking css better

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -4,19 +4,19 @@ const Booking = () => {
   const Book = () => {
     return (
       <div className="flex justify-center items-center mt-8"> {/* Full width */}
-        <div className="w-3/5 h-3/5 p-8 border border-black rounded-lg mr-8 pb-4"> {/* Increased width and added padding bottom */}
+        <div className="w-3/5 h-3/5 p-8 border border-black rounded-lg mr-8 pb-4 border border-gray-400"> {/* Increased width and added padding bottom */}
           <div className="Appointment">
             <div>
               <h1 className="text-2xl font-bold mb-8">Book Appointment</h1> {/* Increased font size */}
             </div>
             <div className="w-1/2 mb-4">
-              <input type="date" placeholder="Choose Date" className="mb-4 border border-black rounded p-2 w-full"></input>
+              <input type="date" placeholder="Choose Date" className="mb-4 border border-black rounded p-2 w-full border border-gray-400 rounded-l-md py-2 px-4 w-96 focus:outline-none focus:ring-1 focus:ring-slate-950"></input>
             </div>
-            <div className="w-1/2 mb-4">
-              <input type="text" placeholder="Input the Google Map Location" className="mb-4 border border-black rounded p-2 w-full"></input>
+            <div className="w-1/2 mb-4 ">
+              <input type="text" placeholder="Input the Google Map Location" className="mb-4 border border-black rounded p-2 w-full border border-gray-400 rounded-l-md py-2 px-4 w-96 focus:outline-none focus:ring-1 focus:ring-slate-950"></input>
             </div>
             <div className="w-1/2 h-48 mb-4">
-              <textarea placeholder="Work Description" className="mb-4 border border-black rounded p-2 w-full h-full"></textarea> {/* Increased height */}
+              <textarea placeholder="Work Description" className="mb-4 w-full h-full mb-4 border border-black rounded p-2 w-full border border-gray-400 rounded-l-md py-2 px-4 w-96 focus:outline-none focus:ring-1 focus:ring-slate-950"></textarea> {/* Increased height */}
             </div>
           </div>
         </div>
