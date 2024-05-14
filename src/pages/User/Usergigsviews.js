e11cc6d96f223b29087bbf68913fce930877b018
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-05-15 03:38:08 +0530

    usergigsview updated

--- src/pages/User/Usergigsviews.js
+++ src/pages/User/Usergigsviews.js
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { useGig } from '../../Context/GigContext';
-import { useNavigate } from 'react-router-dom';
-import Carousel from '../../components/carousel';
-import { getStorage, ref, getDownloadURL } from 'firebase/storage';
-import Navbar from '../../components/nav';
+import React, { useEffect, useState } from "react";
+import { useGig } from "../../Context/GigContext";
+import { useNavigate } from "react-router-dom";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import Navbar from "../../components/nav";
+import Image from "../../images/carpenter.jpg";
 
 const UsergigsViews = () => {
   const { selectedGig } = useGig();
-  const [demoPicsUrls, setDemoPicsUrls] = useState([]);
-  const [gigPdfUrl, setGigPdfUrl] = useState('');
+  const [gigPdfUrl, setGigPdfUrl] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -16,21 +15,16 @@ const UsergigsViews = () => {
       const storage = getStorage();
 
       if (!selectedGig) {
-        console.log('Selected gig not set.');
+        console.log("Selected gig not set.");
         return;
       }
 
-      console.log('Selected gig:', selectedGig);
-
-      // Fetch demo pics URLs
-      const demoPicsUrls = selectedGig.demoPics;
-      console.log('Demo pics URLs:', demoPicsUrls);
-      setDemoPicsUrls(demoPicsUrls);
+      console.log("Selected gig:", selectedGig);
 
       // Fetch gig PDF URL
       const gigPdfRef = ref(storage, `${selectedGig.gigPdf}`);
       const gigPdfUrl = await getDownloadURL(gigPdfRef);
-      console.log('Gig PDF URL:', gigPdfUrl);
+      console.log("Gig PDF URL:", gigPdfUrl);
       setGigPdfUrl(gigPdfUrl);
     };
 
@@ -39,7 +33,7 @@ const UsergigsViews = () => {
 
   const handleBookNow = () => {
     // Navigate to the booking page with service details
-    navigate('/booking', { state: selectedGig });
+    navigate("/booking", { state: selectedGig });
   };
 
   if (!selectedGig) {
@@ -49,36 +43,80 @@ const UsergigsViews = () => {
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
-      <div className="container mx-auto p-8 flex flex-col items-center justify-center h-full">
-        {/* Gig Details Section */}
-        <div className="max-w-md w-full bg-white rounded-lg shadow-md p-6 mb-8">
-          <h1 className="text-2xl font-bold mb-4">{selectedGig.title}</h1>
-          <p className="text-gray-600 mb-2">Category: {selectedGig.category}</p>
-          <p className="text-gray-600 mb-2">Description: {selectedGig.description}</p>
-          <p className="text-gray-600 mb-2">Base Price: Rs.{selectedGig.basePrice} /-</p>
-          <p className="text-gray-600 mb-2">Address: {selectedGig.address}</p>
-          <p className="text-gray-600 mb-2">Phone Number: {selectedGig.phoneNumber}</p>
-          <p className="text-gray-600 mb-4">Email: {selectedGig.email}</p>
-          
-          {/* Gig PDF Section */}
-          <div className="flex justify-between items-center mb-4">
-            <h2 className="text-xl font-semibold">Gig PDF</h2>
-            <a href={gigPdfUrl} target="_blank" rel="noreferrer" download className="text-blue-500 hover:underline">Download Gig PDF</a>
+      <div className="container mx-auto p-8 flex  ">
+        {/* Left Side - 3/5th space */}
+        <div className="md:w-3/5">
+          <div className="w-full  p-6">
+            {/* Gig Details Section */}
+            <div className="mb-8">
+              <br></br>
+              <h1 className="text-2xl font-bold mb-4">{selectedGig.title}</h1>
+              <p className="text-gray-600 mb-2">
+                Category: {selectedGig.category}
+              </p>
+              <p className="text-gray-600 mb-2 ">
+                Description: {selectedGig.description}
+              </p>
+            </div>
+            {/* Display Carpenter Image */}
+            <img
+              src={Image}
+              alt="Carpenter"
+              className="w-full mb-4 rounded-lg"
+            />
           </div>
-          
-          {/* Button to book now */}
-          <button onClick={handleBookNow} className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Book Now
-          </button>
         </div>
-        
-        {/* Display Carousel of Demo Pics */}
-        {/* {demoPicsUrls.length > 0 && (
-          <div className="max-w-md w-full bg-white rounded-lg shadow-md p-6">
-            <h2 className="text-xl font-semibold mb-4">Demo Pics</h2>
-            <Carousel images={demoPicsUrls} />
+
+        {/* Right Side - 2/5th space */}
+        <div className="w-full md:w-2/5 ">
+          <div className="max-w-md w-full p-6">
+            {/* Gig Price Section */}
+            <div className="text-xl bg-white rounded-lg shadow-md font-semibold mb-6 text-center py-8">
+              <h1 className="mb-8">Base Price:</h1>
+              <h2> Rs.{selectedGig.basePrice} /-</h2>
+            </div>
+
+            {/* Button to book now */}
+            <button
+              onClick={handleBookNow}
+              className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-6 rounded"
+            >
+              Book Now
+            </button>
+
+            {/* Gig Contact Section */}
+            <div className="bg-white rounded-lg shadow-md py-4">
+              <div className=" py-5 text-center">
+                <h1 className="mb-8 font-bold">Contact Me</h1>
+                <p className="text-gray-600 mb-4">
+                  Address: {selectedGig.address}
+                </p>
+                <p className="text-gray-600 mb-4">
+                  Phone Number: {selectedGig.phoneNumber}
+                </p>
+                <p className="text-gray-600 ">Email: {selectedGig.email}</p>
+              </div>
+
+              {/* Gig PDF Section */}
+
+              <div className=" text-center mb-4">
+                <h2 className="text-xl font-semibold mb-4">
+                  Police Clearance Certificate
+                </h2>
+                <a
+                  href={gigPdfUrl}
+                  target="_blank"
+                  rel="noreferrer"
+                  download
+                  className="text-blue-500 hover:text-blue-700 mb-4"
+                >
+                  Download PCC PDF
+                </a>
+              </div>
+              <div></div>
+            </div>
           </div>
-        )} */}
+        </div>
       </div>
     </div>
   );
