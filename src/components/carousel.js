ff048f6bdca8d0b1c0938636261b5b8ac123b697
AKSHAY GOPAN <akshaygopan377@gmail.com>
2024-05-14 18:22:51 +0530

    Update carousel.js

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import Navbar from './nav';
 
 
 
 const Carousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [carouselHeight, setCarouselHeight] = useState(0);
 
   const nextSlide = () => {
     setCurrentIndex((prevIndex) =>
@@ -25,13 +27,30 @@ const Carousel = ({ images }) => {
     return () => clearInterval(interval);
   }, [currentIndex]);
 
+  useEffect(() => {
+    const updateCarouselHeight = () => {
+      const navbarHeight = document.querySelector('nav').offsetHeight;
+      setCarouselHeight(window.innerHeight - navbarHeight);
+    };
+
+    window.addEventListener('resize', updateCarouselHeight);
+    updateCarouselHeight();
+
+    return () => {
+      window.removeEventListener('resize', updateCarouselHeight);
+    };
+  }, []);
+
   return (
     <div className="carousel-container">
-      <div className="carousel">
+      <div className="carousel z-20 relative">
         <div
           className="carousel-inner"
           style={{ transform: `translateX(-${currentIndex * 100}%)` }}
         >
+          <div  className="absolute top-0 w-full overflow-hidden z-0" style={{ height: carouselHeight + 'px' }}>
+           <Navbar />
+          </div>
           {images.map((image, index) => (
             <div key={index} className="carousel-slide">
               <img src={image} alt={`Slide ${index + 1}`} />
