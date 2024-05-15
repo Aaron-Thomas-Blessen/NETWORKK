1652727036005c880b1383dc3bd64a2183f76a92
AKSHAY GOPAN <akshaygopan377@gmail.com>
2024-05-16 00:24:49 +0530

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -105,24 +105,4 @@ export default Navbar;
 
 
 
-/*
-const Navbar = () => {
-  return (
-    <nav className="bg-gray-800 text-white flex justify-between items-center p-4">
-      <div className="navbar-logo">
-        <img src="logo.png" alt="Logo" className="h-8" />
-      </div>
-      <ul className="flex gap-4">
-        <li><a href="/" className="hover:text-gray-300">Home</a></li>
-        <li><a href="/about" className="hover:text-gray-300">About</a></li>
-        <li><a href="/services" className="hover:text-gray-300">Services</a></li>
-        <li><a href="/contact" className="hover:text-gray-300">Contact</a></li>
-        <li><a href="/signup" className="hover:text-gray-300">SignUp</a></li>
-        <li><a href="/signin" className="hover:text-gray-300">SignIn</a></li>
-      </ul>
-    </nav>
-  );
-};
 
-export default Navbar;
-*/
