c154923749bebbcc7da662a543519531a79b4f3a
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-05-15 10:20:44 +0530

    profile dashboard css updated

--- src/pages/Seller/ProfileDashboardPage.js
+++ src/pages/Seller/ProfileDashboardPage.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { useGig } from '../../Context/GigContext';
-import Carousel from '../../components/carousel';
-import { getStorage, ref, getDownloadURL } from 'firebase/storage';
-import { useEffect, useState } from 'react';
-import { useUser } from '../../Context/Context';
+import React from "react";
+import { useGig } from "../../Context/GigContext";
+import Carousel from "../../components/carousel";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import { useEffect, useState } from "react";
+import { useUser } from "../../Context/Context";
 import Navbar from "../../components/nav";
+import Image from "../../images/carpenter.jpg";
 
 const ProfileDashboard = () => {
   const { selectedGig } = useGig();
   const { user } = useUser();
   const [demoPicsUrls, setDemoPicsUrls] = useState([]);
-  const [gigPdfUrl, setGigPdfUrl] = useState('');
+  const [gigPdfUrl, setGigPdfUrl] = useState("");
 
   useEffect(() => {
     const fetchUrls = async () => {
@@ -36,35 +37,81 @@ const ProfileDashboard = () => {
   }
 
   return (
-    <div>
-      <Navbar currentPage="ProfileDashboardPage" />
-    
-    <div className="container mx-auto px-4 py-8">
-      
-      <div className="max-w-4xl  mx-auto">
-        <h1 className="text-3xl font-bold mb-4">{selectedGig.title}</h1>
-        <p className="text-lg mb-4"><span className="font-semibold">Category:</span> {selectedGig.category}</p>
-        <p className="text-lg mb-4"><span className="font-semibold">Description:</span> {selectedGig.description}</p>
-        <p className="text-lg mb-4"><span className="font-semibold">Base Price:</span> Rs.{selectedGig.basePrice} /-</p>
-        <p className="text-lg mb-4"><span className="font-semibold">Address:</span> {selectedGig.address}</p>
-        <p className="text-lg mb-4"><span className="font-semibold">Phone Number:</span> {selectedGig.phoneNumber}</p>
-        <p className="text-lg mb-4"><span className="font-semibold">Email:</span> {selectedGig.email}</p>
-        {/* <div className="mb-8">
-          <h2 className=" font-semibold mb-4">Demo Pics</h2>
-          <Carousel images={demoPicsUrls} />
-        </div> */}
-        <div>
-          <h2 className="text-2xl font-semibold mb-4">Gig PDF</h2>
-          <a href={gigPdfUrl} target="_blank" rel="noreferrer" download className="text-blue-500 hover:underline">Download Gig PDF</a>
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <div className="container mx-auto p-8 flex  ">
+        {/* Left Side - 3/5th space */}
+        <div className="md:w-3/5">
+          <div className="w-full  p-6">
+            {/* Gig Details Section */}
+            <div className="mb-8">
+              <br></br>
+              <h1 className="text-2xl font-bold mb-4">{selectedGig.title}</h1>
+              <p className="text-gray-600 mb-2">
+                Category: {selectedGig.category}
+              </p>
+              <p className="text-gray-600 mb-2 ">
+                Description: {selectedGig.description}
+              </p>
+            </div>
+            {/* Display Carpenter Image */}
+            <img
+              src={Image}
+              alt="Carpenter"
+              className="w-full mb-4 rounded-lg"
+            />
+          </div>
+        </div>
+
+        {/* Right Side - 2/5th space */}
+        <div className="w-full md:w-2/5 ">
+          <div className="max-w-md w-full p-6">
+            {/* Gig Price Section */}
+            <div className="text-xl bg-white rounded-lg shadow-md font-semibold mb-6 text-center py-8">
+              <h1 className="mb-8">Base Price:</h1>
+              <h2> Rs.{selectedGig.basePrice} /-</h2>
+            </div>
+
+            {/* Gig Contact Section */}
+            <div className="bg-white rounded-lg shadow-md mb-8 py-4">
+              <div className=" py-5 text-center">
+                <h1 className="mb-8 font-bold">Contact Me</h1>
+                {/* <p>First Name: {userData.firstName}</p> */}
+                <p className="text-gray-600 mb-4">
+                  Address: {selectedGig.address}
+                </p>
+                <p className="text-gray-600 mb-4">
+                  Phone Number: {selectedGig.phoneNumber}
+                </p>
+                <p className="text-gray-600 ">Email: {selectedGig.email}</p>
+              </div>
+            </div>
+            {/* Gig PDF Section */}
+            <div className="bg-white rounded-lg shadow-md py-4">
+              <div className=" text-center mb-4">
+                <h2 className="text-xl font-semibold mb-4">
+                  Police Clearance Certificate
+                </h2>
+                <a
+                  href={gigPdfUrl}
+                  target="_blank"
+                  rel="noreferrer"
+                  download
+                  className="text-blue-500 hover:text-blue-700 mb-4"
+                >
+                  Download PCC PDF
+                </a>
+              </div>
+              <div></div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
 
 export default ProfileDashboard;
-
 
 /*import React from 'react';
 import Navbar from '../components/nav';
