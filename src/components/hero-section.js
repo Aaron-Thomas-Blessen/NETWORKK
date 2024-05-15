6ed3ae01b2b9609db4305fd4699e01c47a8a2856
AKSHAY GOPAN <akshaygopan377@gmail.com>
2024-05-15 23:38:52 +0530

    Update hero-section.js

--- src/components/hero-section.js
+++ src/components/hero-section.js
@@ -5,26 +5,34 @@ import Navbar from "./nav";
 const HeroSection = () => {
   return (
     <>
-    <Navbar />
+    
     <div className="overflow-hidden">
       <div className=" w-screen overflow-hidden ">
         <img
-          src="https://firebasestorage.googleapis.com/v0/b/network-c70d4.appspot.com/o/login%2Fhero-section.jpg?alt=media&token=673281fe-b61b-4f7b-909b-a59978974130"
+          src="https://firebasestorage.googleapis.com/v0/b/network-c70d4.appspot.com/o/login%2Fpexels-ono-kosuki-5974053.jpg?alt=media&token=61bd4e41-6f9b-4765-9ff3-16d7154478fc"
           alt="Placeholder"
           className="w-full h-screen overflow-hidden"
         />
-        <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-          <h1 className="text-4xl font-bold text-white text-center">Welcome to Your App</h1>
+        <div className="absolute top-0 left-0 w-1/2 h-screen flex flex-col justify-center lg:ml-16 items-start">
+          <h1 className="text-4xl font-bold text-blue-gray-900 text-left">Find the right skilled service at <span><img src="https://firebasestorage.googleapis.com/v0/b/network-c70d4.appspot.com/o/login%2Flogo-text1__1_-removebg-preview.png?alt=media&token=ff8304f7-1b98-4d3d-92dc-af6d60f928dc" className="w-1/2"/></span></h1>
+          <div className="w-full flex flex-row " >
+        <input
+          type="text"
+          placeholder="What are you looking for..."
+          className="w-8/12 h-16 mt-7 relative px-4 py-4 mb-4 border rounded-l-md"
+        />
+        <button class=" w-2/12 h-16  mt-7 align-middle relative bg-gray-900 text-white rounded-r-md border border-blue-gray-300">
+              Search
+            </button>
         </div>
+        </div>
+        
       </div>
 
       {/* Nav */}
       
 
-      {/* Inputs */}
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white bg-opacity-80 p-8 rounded-md shadow-md">
-        
-      </div>
+      
     </div>
     </>
   );
